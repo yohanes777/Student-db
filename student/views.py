0f1fd756d5f379328157fd7c46016842d72e3b50
Ivan <irieznik7@gmail.com>
2016-07-07 17:40:42 +0300

    App

--- student/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def students_list(request):
-    return render(request, 'students/students_list.html', {})
--- students/apps.py
+++ students/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 
 class StudentConfig(AppConfig):
-    name = 'student'
+    name = 'students'
