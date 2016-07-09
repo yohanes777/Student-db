bc472abf335022c6b52f75d92cb8938ff659deb3
Ivan <irieznik7@gmail.com>
2016-07-09 14:02:57 +0300

    App

--- students/models.py
+++ students/models.py
@@ -1,5 +1,46 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+
+    first_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name="Ім'я"
+    )
+    last_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name="Прізвище"
+    )
+    middle_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name="По-батькові",
+        default=''
+    )
+    birthday = models.DateField(
+        blank=False,
+        verbose_name="Дата народження",
+        null=True
+    )
+    photo = models.ImageField(
+        blank=True,
+        verbose_name="Фото",
+        null=True
+    )
+    ticket = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name="Білет"
+    )
+    notes = models.TextField(
+        blank=True,
+        verbose_name="Додатково"
+    )
+
+
+
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -12,7 +12,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+PORTAL_URL = 'http://localhost:8000'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -61,6 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentsdb.context_processors.student_proc',
             ],
         },
     },
