e2c0811d64531bada9f23449da64e279a8da1d5c
Ivan <irieznik7@gmail.com>
2016-07-11 16:18:09 +0300

    App

--- students/models.py
+++ students/models.py
@@ -6,41 +6,48 @@ from django.db import models
 
 class Student(models.Model):
 
+    class Meta(object):
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенти"
+
     first_name = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name="Ім'я"
-    )
+        verbose_name="Ім'я")
+
     last_name = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name="Прізвище"
-    )
+        verbose_name="Прізвище")
+
     middle_name = models.CharField(
         max_length=256,
-        blank=False,
+        blank=True,
         verbose_name="По-батькові",
-        default=''
-    )
+        default='')
+
     birthday = models.DateField(
         blank=False,
         verbose_name="Дата народження",
-        null=True
-    )
+        null=True)
+
     photo = models.ImageField(
         blank=True,
         verbose_name="Фото",
-        null=True
-    )
+        null=True)
+
     ticket = models.CharField(
         max_length=256,
         blank=False,
-        verbose_name="Білет"
-    )
+        verbose_name="Білет")
+
+
     notes = models.TextField(
         blank=True,
-        verbose_name="Додатково"
-    )
+        verbose_name="Додатково")
+
+    def __unicode__(self):
+        return "%s %s" % (self.first_name, self.last_name)
 
 
 
